b60951add80c79a93fc3fdb675ad35d1a7479be0
DmitriyKarpovskiy <dmkarpovskiy@gmail.com>
2017-07-11 12:56:40 +0300

    first

---
+++ public/js/registration.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $('#register-href').click(function(){
+//         $("#content").empty();
+//         showContent();
+//     });
+// });
+//
+// function showContent(){
+//     $.ajax({
+//         method: 'POST',
+//         url: "registration.blade.php",
+//         cache: false,
+//         success: function(html){
+//             $("#content").html(html);
+//         }
+//     });
+// }
---
+++ resources/views/rigistration.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <script src=""></script>
+    </head>
+
+    <body>
+        <h1>Dimka</h1>
+    </body>
+</html>
